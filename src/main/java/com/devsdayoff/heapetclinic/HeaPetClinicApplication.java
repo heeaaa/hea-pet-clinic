899be4927c3ebff0dd4c30a24625ce9506296f5b
habejo <abejohanna@gmail.com>
2021-03-21 18:59:25 +0800

    Initial Project Add

---
+++ src/main/java/com/devsdayoff/heapetclinic/HeaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.devsdayoff.heapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeaPetClinicApplication.class, args);
+	}
+
+}
