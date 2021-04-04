56034ffad3f91c84c97bf926a8835561941c1552
Hanna A <abejohanna@gmail.com>
2021-04-04 16:21:56 +0800

    Impl map-based services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/devsdayoff/heapetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.devsdayoff.heapetclinic.service.map;
+
+import com.devsdayoff.heapetclinic.model.Vet;
+import com.devsdayoff.heapetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+}
