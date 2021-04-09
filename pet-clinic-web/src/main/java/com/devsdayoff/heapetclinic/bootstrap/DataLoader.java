c1c83ef503a1663802af39a3b91a3c6be89814d3
Hanna A <abejohanna@gmail.com>
2021-04-09 20:31:36 +0800

    Refactor map impl to manage ID property. Closes #23

--- pet-clinic-web/src/main/java/com/devsdayoff/heapetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/devsdayoff/heapetclinic/bootstrap/DataLoader.java
@@ -23,25 +23,21 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Hanna");
         owner1.setLastName("Abejo");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Harold");
         owner2.setLastName("Abejo");
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
-        owner3.setId(3L);
         owner3.setFirstName("Hardy");
         owner3.setLastName("Abejo");
         ownerService.save(owner3);
 
         Owner owner4 = new Owner();
-        owner4.setId(4L);
         owner4.setFirstName("Roxan");
         owner4.setLastName("Abejo");
         ownerService.save(owner4);
@@ -49,13 +45,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Mikasa");
         vet1.setLastName("Ackerman");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Eren");
         vet2.setLastName("Jaeger");
         vetService.save(vet2);
